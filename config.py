2a0828441df82dc04621a9d889bd429ea8c49ed5
Roman <roma55028@gmail.com>
2021-09-06 13:06:16 +0300

    API key changed

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = '1969894051:AAHKuaIxULTmB3OwEOLgXIStM1-Rrs-LWyA'
 exchangeratesapi_key = "4fcd1ee27ee4fa184ee49062d72cc9a8"
-fastforex_key = "62dd97e8a0-a0b76115ed-qyegpy"
+fastforex_key = "cc2e33f8a9-65c840de57-qz0c11"
